1d09ca9a60400178cd8a6831f0945986d4e7cee9
Sergey Atroshchenko <kapxapot@gmail.com>
2023-03-08 18:44:13 +0400

    One more

--- components/clock-card.tsx
+++ components/clock-card.tsx
@@ -97,7 +97,7 @@ export function ClockCard(props: Props) {
 
   return (
     <>
-      <Card className="min-w-[165px]">
+      <Card className="min-w-[160px]">
         {clock.default ? staticTitle() : menu()}
         <div className="text-indigo-500 text-5xl -mt-1">
           {format(tzNow(clock.timeZone), 'HH:mm')}
