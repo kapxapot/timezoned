57ad59b41ee20127f58ff237e181e2870f67d1f5
Sergey Atroshchenko <kapxapot@gmail.com>
2023-04-24 12:55:28 +0400

    Formatting

--- lib/clock.tsx
+++ lib/clock.tsx
@@ -1,5 +1,5 @@
-import {v4 as uuidv4} from 'uuid';
-import { extractCity } from './timezones';
+import { v4 as uuidv4 } from "uuid";
+import { extractCity } from "./timezones";
 
 export interface IClock {
   timeZone: string;
